c52c0d7528b6ddedbcd1202e875dcfaf21161631
SamplesMaker <insert@kakao.com>
2020-02-23 17:18:23 +0900

    SwiftUI EnvirenmentObject Sample

--- EnvironmentObjectSample/ContentView.swift
+++ EnvironmentObjectSample/ContentView.swift
@@ -8,14 +8,41 @@
 
 import SwiftUI
 
+class GameSettings: ObservableObject {
+    @Published var score: Int = 0
+}
+
+
 struct ContentView: View {
+    
+    @EnvironmentObject var settings: GameSettings
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView {
+            VStack {
+                Stepper(value: $settings.score, in: 1...100000, step: 1000, label: {
+                    Text("현재 스코어: \(settings.score)")
+                }).padding()
+                
+                NavigationLink(destination: ScoreView()) {
+                    Text("보여줘 너의 스코어")
+                }
+            }
+        }
+    }
+}
+
+struct ScoreView: View {
+    @EnvironmentObject var settings: GameSettings
+    
+    var body: some View {
+        Text("너의 스코어 : \(settings.score)")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(GameSettings())
     }
 }
--- EnvironmentObjectSample/SceneDelegate.swift
+++ EnvironmentObjectSample/SceneDelegate.swift
@@ -12,6 +12,8 @@ import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    
+    var settings = GameSettings()
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -25,7 +27,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            // window.rootViewController = UIHostingController(rootView: contentView)
+            
+            window.rootViewController = UIHostingController(rootView: contentView.environmentObject(settings))
+            
             self.window = window
             window.makeKeyAndVisible()
         }
